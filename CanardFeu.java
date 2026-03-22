a185bd5318e1234342d5f92bb1de6db00997a0b0
Arthur Gaussot <gaussota@3il.fr>
2026-03-22 23:53:22 +0100

    Feat : Classe CanardFeu

---
+++ CanardFeu.java
@@ -0,0 +1,45 @@
+class CanardFeu extends CanardDeCombat {
+    private double intensiteFlamme;
+
+    
+    public CanardFeu(String nom, int pvMax, int atk, double intensiteFlamme) {
+        super(nom, pvMax, atk);
+        if (intensiteFlamme < 0.8 || intensiteFlamme > 1.5) {
+            throw new IllegalArgumentException("L'intensité doit être entre 0.8 et 1.5");
+        }
+        this.intensiteFlamme = intensiteFlamme;
+    }
+
+    
+    public CanardFeu(String nom, int pvMax, int atk) {
+        this(nom, pvMax, atk, 1.0); // Intensité de 1.0 par défaut
+    }
+
+    @Override
+    public String getType() {
+        return "Feu";
+    }
+
+    
+    @Override
+    public void attaquer(CanardDeCombat cible) {
+        if (this.estKO() || cible.estKO()) return;
+
+        System.out.println(this.getSurnom() + " attaque " + cible.getSurnom() + " !");
+
+        // Utilisation de l'intensité de la flamme dans le calcul
+        double multiplicateur = calculerMultiplicateurType(this.getType(), cible.getType());
+        int degatsFinaux = (int) (this.getAtk() * multiplicateur * this.intensiteFlamme);
+
+        if (multiplicateur > 1.0) System.out.println("C'est super efficace !");
+        else if (multiplicateur < 1.0 && multiplicateur > 0) System.out.println("Ce n'est pas très efficace...");
+        else if (multiplicateur == 0) System.out.println("Cela n'a aucun effet...");
+
+        cible.subirDegats(degatsFinaux);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " [Intensité: " + this.intensiteFlamme + "]";
+    }
+}
