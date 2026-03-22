be162c544694fb39943a14761b878d6fb2c7c723
Arthur Gaussot <gaussota@3il.fr>
2026-03-22 23:55:30 +0100

    Feat : Classe CanardPlante.java

---
+++ CanardPlante.java
@@ -0,0 +1,28 @@
+class CanardPlante extends CanardDeCombat {
+
+    
+    public CanardPlante(String nom, int pvMax, int atk) {
+        super(nom, pvMax, atk);
+    }
+
+    
+    public CanardPlante(String nom) {
+        this(nom, 80, 8); // Stats par défaut d'un canard plante
+    }
+
+    @Override
+    public String getType() {
+        return "Plante";
+    }
+
+    public void regenerer() {
+        if (!this.estKO()) {
+            int soin = (int) (this.getPvMax() * 0.10);
+            
+            
+            this.gagnerPv(soin); 
+            
+            System.out.println(this.getSurnom() + " se régénère de " + soin + " PV !");
+        }
+    }
+}
