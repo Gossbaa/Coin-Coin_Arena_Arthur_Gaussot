09a9145e93c170008cfc3e28d93a597b55d95ce6
Arthur Gaussot <gaussota@3il.fr>
2026-03-23 09:33:38 +0100

    Feat : Implémentation des interfaces Soignable et Combattant

--- CanardDeCombat2.java
+++ CanardDeCombat2.java
@@ -1,4 +1,20 @@
-public abstract class CanardDeCombat {
+public interface Soignable {
+    void soigner();
+    int getPvActuels();
+    int getPvMax();
+    
+    default double pourcentagePV() {
+        return (double) getPvActuels() / getPvMax() * 100;
+    }
+}
+
+public interface Combattant {
+    void attaquer(CanardDeCombat cible);
+    boolean estKO();
+    String getNom();
+}
+
+public abstract class CanardDeCombat implements Soignable, Combattant {
 
     public enum TypeElementaire {
         FEU, EAU, PLANTE, NORMAL;
@@ -29,15 +45,26 @@ public abstract class CanardDeCombat {
         nbCanardsCrees++;
     }
 
+    @Override
     public String getNom() { return nom; }
+    
     public String getSurnom() { return surnom; }
+    
+    @Override
     public int getPvMax() { return pvMax; }
+    
+    @Override
     public int getPvActuels() { return pvActuels; }
+    
     public int getAtk() { return atk; }
+    
     public static int getNbCanardsCrees() { return nbCanardsCrees; }
+    
     public void setSurnom(String surnom) { this.surnom = surnom; }
     
+    @Override
     public boolean estKO() { return this.pvActuels <= 0; }
+    
     public abstract TypeElementaire getType();
 
     public void subirDegats(int degats) {
@@ -47,13 +74,18 @@ public abstract class CanardDeCombat {
         System.out.println(this.surnom + " subit " + degats + " dégâts ! (PV: " + this.pvActuels + "/" + this.pvMax + ")");
     }
 
-    //Double Dispatch
+    @Override
+    public void soigner() {
+        this.pvActuels = this.pvMax;
+        System.out.println(this.surnom + " a été soigné et récupère tous ses PV !");
+    }
+
+    // Double Dispatch
     public double etreAttaqueePar(CanardFeu attaquant) { return 1.0; }
     public double etreAttaqueePar(CanardEau attaquant) { return 1.0; }
     public double etreAttaqueePar(CanardPlante attaquant) { return 1.0; }
     public double etreAttaqueePar(CanardClassique attaquant) { return 1.0; }
 
-    
     protected void effectuerAttaque(CanardDeCombat cible, double mult) {
         int degats = (int)(getAtk() * mult);
         System.out.println(getSurnom() + " attaque " + cible.getSurnom() 
@@ -62,6 +94,6 @@ public abstract class CanardDeCombat {
         cible.subirDegats(degats);
     }
 
-    
+    @Override
     public abstract void attaquer(CanardDeCombat cible);
 }
